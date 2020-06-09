8151672d5ff5af2f26e9344d66c7ba9018d67f6f
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-06-09 17:32:26 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.2]
-  def change_column
-    add_column :students, :birthdate, :datetime
+  def change
+    change_column :students, :birthdate, :datetime
   end
 end
